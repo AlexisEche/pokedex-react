1e6fbecb3b417f2f5bf5e367497f80c10bc674aa
joeleche <joel.eche5@gmail.com>
2020-10-03 23:37:56 -0500

    Header for the pokemon section

---
+++ src/components/HeaderPokemon.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './../styles/HeaderPokemon.sass';
+
+export const HeaderPokemon = ({ number, name, mainType }) => {
+  return (
+    <div className="container is-fullhd mb4">
+      <div className="notification is-primary not-rounded pb0">
+        <div className="columns">
+          <div className="column is-offset-1 is-3 line-header">
+            <h5 className="title is-5">N° {number}</h5>
+          </div>
+          <div className="column is-4 line-header">
+            <h6 className="title is-6">{name}</h6>
+          </div>
+          <div className="column is-3 line-header">
+            <span>{mainType}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+HeaderPokemon.propTypes = {
+  number: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  mainType: PropTypes.string.isRequired,
+}
